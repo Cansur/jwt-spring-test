79db17c64faf6c79179606798c0b41b5bb5eaa88
Cansur <tjswo0682@gmail.com>
2024-09-08 21:10:01 +0900

    first commit

---
+++ src/main/java/test/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String getMethodName() {
+        return "index.html";
+    }
+    
+}
